682183c79b9445814aba20c8d30b3bb96c62194d
Adenikym <mm5adeniky@gmail.com>
2020-08-15 18:50:46 +0100

    responsive

--- portal.js
+++ portal.js
@@ -6,10 +6,12 @@ function viewMore () {
     var hide=document.getElementById("viewmore");
     if(hide.style.display==="none") {
         hide.style.display="block"
+        document.getElementById("vbutton").innerHTML="View Less"
     }
 
     else{
         hide.style.display="none"
+        document.getElementById("vbutton").innerHTML="View More"
     }
 
 
