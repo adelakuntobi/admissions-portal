3218430fd6ed9f8f82444c08722c1fb6d9874fee
Adenikym <mm5adeniky@gmail.com>
2020-08-15 18:18:14 +0100

    new

---
+++ portal.js
@@ -0,0 +1,20 @@
+
+document.getElementById("vbutton").addEventListener("click",viewMore);
+
+
+function viewMore () {
+    var hide=document.getElementById("viewmore");
+    if(hide.style.display==="none") {
+        hide.style.display="block"
+    }
+
+    else{
+        hide.style.display="none"
+    }
+
+
+}
+
+
+
+
