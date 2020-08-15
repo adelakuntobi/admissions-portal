99a586c9c0a7bc2891a7aeeb799b98c96bb625bb
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-08-15 12:14:41 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,22 @@ var openButton = document.getElementById("openButton");
 //Getting the button that opens then the modal
 var closeBtn = document.getElementById("closeBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByTagName('span')[0];
-
-//On click of the close icon, the modal dissappears
-span.addEventListener('click',displayModal);
 // When the user clicks the button, open the modal 
-openButton.onclick = function() {
-  modal.style.display = "block";
+openButton.onclick = function () {
 }
+openButton.addEventListener("click", () => {
+  modal.style.display = "block";
+})
+
 
 // Function that removes the modal i.e makes the display none
-function displayModal() {
+
+closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
-}
+})
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
